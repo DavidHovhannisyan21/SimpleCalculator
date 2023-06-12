cb73e6efe688134b91e89f3ee7ccd3519321c04d
DavidHovhannisyan21 <davo.hovhannisyan2001@gmail.com>
2023-06-12 16:08:48 +0400

    Simple Calc

---
+++ CalcApp/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <Calculator></Calculator>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+export default{
+  components:{
+    Calculator
+  }
+}
+</script>
